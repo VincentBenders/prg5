0e17762dbf68ddd49451e0943b94de085fc56dbf
Vincent Benders <74290916+VincentBenders@users.noreply.github.com>
2024-10-16 17:16:13 +0200

    made a one to many connection with color in card model.

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Card extends Model
 {
-
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Color::class);
+    }
 }
