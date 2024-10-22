3388862deb43aadf97b35b40b3b8ad43dd9c1ee0
Vincent Benders <74290916+VincentBenders@users.noreply.github.com>
2024-10-22 14:07:59 +0200

    fixed multiple navbars

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,9 @@
-
+<x-layout>
 @foreach($cards as $card)
-    <x-layout>
+
         <h1>{{ $card->name }}</h1>
         <h2>{{ $card->type }}</h2>
         <p>{{ $card->description }}</p>
-    </x-layout>
+
 @endforeach
+</x-layout>
