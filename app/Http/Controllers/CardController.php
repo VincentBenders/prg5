251793f39f41ff34080e4564b8d09d684c9a61a9
Vincent Benders <74290916+VincentBenders@users.noreply.github.com>
2024-10-26 20:50:59 +0200

    made a working form to add cards to db.

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -32,7 +32,41 @@ class CardController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //get all inputs
+    //save in record
+        $card = new Card;
+
+        $card->name = $request->input('name');
+        $card->description = $request->input('description');
+        $card->type = $request->input('type');
+        $card->color_id = $request->input('color');
+        $card->power = $request->input('power');
+        $card->toughness = $request->input('toughness');
+        $card->blue = $request->input('blue');
+        $card->black = $request->input('black');
+        $card->green = $request->input('green');
+        $card->red = $request->input('red');
+        $card->white = $request->input('white');
+        $card->colorless = $request->input('colorless');
+        $card->generic = $request->input('generic');
+
+//        $card->name = 'test';
+//        $card->description = 'test';
+//        $card->type = 'test';
+//        $card->color_id = 1;
+//        $card->power = 1;
+//        $card->toughness = 1;
+//        $card->blue = 1;
+//        $card->black = 1;
+//        $card->green = 1;
+//        $card->red = 1;
+//        $card->white = 1;
+//        $card->colorless = 1;
+//        $card->generic = 1;
+
+        $card->save();
+        return redirect('/');
+
 
     }
 
@@ -67,5 +101,7 @@ class CardController extends Controller
     public function destroy(Card $card)
     {
         //
+        $card->delete();
+        return redirect('/');
     }
 }
--- resources/views/cards/create.blade.php
+++ resources/views/cards/create.blade.php
@@ -1,28 +1,69 @@
 <x-app-layout>
     <!-- Simplicity is the essence of happiness. - Cedric Bledsoe -->
     <form action="{{ route('cards.store') }}" method="POST" class="dark:text-white">
+        @csrf
 {{--        @method('put')--}}
         <div>
-            <p>Name</p>
+            <p class="text-xl">Name</p>
             <input type="text" name="name" id="" class="dark:bg-gray-800">
         </div>
         <div>
-            <p>Description</p>
+            <p class="text-xl">Description</p>
             <input type="text" name="description" id="" class="dark:bg-gray-800">
         </div>
         <div>
-            <p>Type</p>
+            <p class="text-xl">Type</p>
             <input type="text" name="type" id="" class="dark:bg-gray-800">
         </div>
         <div>
-            <p>Color</p>
+            <p class="text-xl">Power</p>
+            <input type="text" name="power" id="" class="dark:bg-gray-800" value="0">
+        </div>
+        <div>
+            <p class="text-xl">Toughness</p>
+            <input type="text" name="toughness" id="" class="dark:bg-gray-800" value="0">
+        </div>
+        <div>
+            <p class="text-xl">Color</p>
             <select id="color" name="color" class="dark:bg-gray-800">
-                <option value="red">red</option>
-                <option value="blue">blue</option>
-                <option value="green">green</option>
-                <option value="black">black</option>
-                <option value="white">white</option>
+                <option value="1">blue</option>
+                <option value="2">black</option>
+                <option value="3">green</option>
+                <option value="4">red</option>
+                <option value="5">white</option>
+                <option value="6">colorless</option>
             </select>
+        </div>
+        <div>
+            <p class="text-xl">Mana</p>
+            <div>
+                <p>Blue</p>
+                <input type="number" name="blue" id="" class="dark:bg-gray-800" value="0">
+            </div>
+            <div>
+                <p>Black</p>
+                <input type="number" name="black" id="" class="dark:bg-gray-800" value="0">
+            </div>
+            <div>
+                <p>Green</p>
+                <input type="number" name="green" id="" class="dark:bg-gray-800" value="0">
+            </div>
+            <div>
+                <p>Red</p>
+                <input type="number" name="red" id="" class="dark:bg-gray-800" value="0">
+            </div>
+            <div>
+                <p>White</p>
+                <input type="number" name="white" id="" class="dark:bg-gray-800" value="0">
+            </div>
+            <div>
+                <p>Colorless</p>
+                <input type="number" name="colorless" id="" class="dark:bg-gray-800" value="0">
+            </div>
+            <div>
+                <p>Generic</p>
+                <input type="number" name="generic" id="" class="dark:bg-gray-800" value="0">
+            </div>
         </div>
         <div>
         <input type="submit" class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
